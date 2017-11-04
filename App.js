6c1417a962f17a00d51f6110f4e9e69ff539c1ce
Jayasim <jayasim@gmail.com>
2017-11-04 23:17:24 +0530

    Listed stack screens in App index

--- App.js
+++ App.js
@@ -1,23 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { Text, View, StatusBar, Dimensions } from "react-native"
+import styled from "styled-components/native"
+import { Button, Spinner } from "nachos-ui"
+import reducer from "./reducers"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+
+const OnboardingScreen = {
+  OnBoarding: {
+    screen: OnBoarding,
+    navigationOptions: {
+      header: null
+    }
+  }
+}
+
+const StackScreens = {
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      header: null
+    }
+  },
+  DeckDetail: {
+    screen: DeckDetailScreen,
+    navigationOptions: {
+      header: null,
+      title:"Deck Detail"
+    }
+  },
+  NewQuestion: {
+    screen: NewQuestionScreen,
+    header: {
+      title: <Text>Add a New Question</Text>
+    }
+  },
+  NewDeck: {
+    screen: NewDeckScreen
+  }
+}
+
+let AllScreens = false
+let MainNavigator = false
+
+API.getUnboardingSeen().then(seen => {
+  if (seen) {
+    AllScreens = StackScreens
+  } else {
+    AllScreens = Object.assign(OnboardingScreen, StackScreens)
+  }
+  MainNavigator = StackNavigator(AllScreens)
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>{AllScreens && <MainNavigator />}</View>
+      </Provider>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
